b60ab09e6750c84346cf2be2cc40d7f938472e67
Enrique Javier Navarro Jover <87661925+Kikee282@users.noreply.github.com>
2026-07-28 15:19:00 +0200

    Add meta tags and links for PWA support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,12 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="manifest" href="/manifest.json">
+        <meta name="theme-color" content="#2563eb">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <link rel="apple-touch-icon" href="/img/logo.png">
+            
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
